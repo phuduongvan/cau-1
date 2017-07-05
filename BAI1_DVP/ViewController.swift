ff44fb03a35e71d15672ad8e6a1d093d30114d5b
Cntt21 <Admin21@Cntt21s-iMac.local>
2017-07-05 15:41:17 +0700

    Initial Commit

---
+++ BAI1_DVP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BAI1_DVP
+//
+//  Created by Cntt21 on 7/5/17.
+//  Copyright © 2017 Cntt21. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
